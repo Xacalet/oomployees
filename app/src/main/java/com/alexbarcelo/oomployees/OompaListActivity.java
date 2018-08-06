57df22d56de467bef6446ca2748c627f9ae647e4
Xacalet <alex.barcelo@outlook.com>
2018-08-06 12:31:29 +0200

    Default project structure

---
+++ app/src/main/java/com/alexbarcelo/oomployees/OompaListActivity.java
@@ -0,0 +1,13 @@
+package com.alexbarcelo.oomployees;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OompaListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oompa_list);
+    }
+}
